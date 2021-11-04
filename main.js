226ce262a3f6e7d29bc81f84eba1541f36d1f363
Ihor Aksunov <Aksunovihor@gmail.com>
2021-11-04 16:53:54 +0200

    call update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strick';
 
-//custom Bind
+//custom Bind, Call
 Function.prototype.customBind = function(thisArg, ...args){
     let symbol = Symbol("function");
     let object = Object.assign(Object.assign({}, thisArg), {[symbol] : this});
@@ -10,7 +10,6 @@ Function.prototype.customBind = function(thisArg, ...args){
     };
 };
 
-//custom Call
 Function.prototype.customCall = function(thisArg, ...args){
     let symbol = Symbol("function");
     let object = Object.assign(Object.assign({}, thisArg), {[symbol] : this});
@@ -21,14 +20,13 @@ Function.prototype.customCall = function(thisArg, ...args){
 };
 
 //Array methods
-//custom ForEach
+
 Array.prototype.customForEach = function (compare) {
     for (let i = 0; i < this.length; i++) {
         compare(this[i], i, this);
     }
 };
 
-//custom Map
 Array.prototype.customMap = function (compare) {
     let result = [];
 
@@ -39,7 +37,6 @@ Array.prototype.customMap = function (compare) {
     return result.splice(0, this.length);
 };
 
-//custom Fillter
 Array.prototype.customFillter = function (compare) {
     let result = [];
 
@@ -52,7 +49,6 @@ Array.prototype.customFillter = function (compare) {
     return result;
 };
 
-//custom Reduce
 Array.prototype.customReduce = function (compare, accum) {
     accum = accum || 0;
 
@@ -66,7 +62,7 @@ Array.prototype.customReduce = function (compare, accum) {
     return accum;
 };
 
-//fibonacci Iterator
+//fibonacci Iterator, Generator
 let fibonachiIterator = {
     a: 0,
     b: 1,
